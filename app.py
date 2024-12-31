304d64865be80707088c775f6138e27048361113
Angel de la cruz <angelmudc14@gmail.com>
2024-12-31 12:09:49 -0400

    Ajustando app.run para Heroku

--- app.py
+++ app.py
@@ -860,4 +860,4 @@ def referencias():
     return render_template('referencias.html', datos_candidata=datos_candidata, mensaje=mensaje)
 
 Si  if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+       app.run(debug=True)
