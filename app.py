e0fa03b9684e4f20c2244e4d5546338c9ad2bc4c
Angel de la cruz <angelmudc14@gmail.com>
2025-01-02 17:25:14 -0400

    Ajustes para despliegue en Heroku

--- app.py
+++ app.py
@@ -860,4 +860,5 @@ def referencias():
     return render_template('referencias.html', datos_candidata=datos_candidata, mensaje=mensaje)
 
 if __name__ == "__main__":
-   app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
